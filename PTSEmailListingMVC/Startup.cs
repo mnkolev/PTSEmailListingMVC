5039874b0dded80b43e4cebf8a557a6fbd49e9c9
mkghost <mkghost77@gmail.com>
2015-10-04 22:48:06 -0400

    Init Commit

---
+++ PTSEmailListingMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PTSEmailListingMVC.Startup))]
+namespace PTSEmailListingMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
